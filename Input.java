a529a13f6f35de15762347a9712237d69ed5da8b
ashish229 <72303789+ashish229@users.noreply.github.com>
2020-10-07 21:41:22 +0530

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		String name;
+		Scanner s = new Scanner (System.in);
+		System.out.print("Enter your name :- ");
+		name = s.nextLine();
+		System.out.println("Hello "+name+" Welcome to Java");
+	}
+}
